0589b8d64a8d295355a9c85b376e0dc07141ac3e
vishalp <86771384+vishal271099@users.noreply.github.com>
2023-10-10 11:58:34 +0530

    Update settings.py

--- Cars/Cars/settings.py
+++ Cars/Cars/settings.py
@@ -161,13 +161,6 @@ ADMIN_REORDER = [
     )}
 ]
 
-#  Send Email related setting--------------------------------------- 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'panchalvishal2710@gmail.com'
-EMAIL_HOST_PASSWORD = 'vs150379'
 
 # Celery task setting-----------------------------------------
 # CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
